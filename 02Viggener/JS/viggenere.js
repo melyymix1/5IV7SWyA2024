6f8e5c35dabedc355a47e55271da264f5db4db2e
melyymix1 <mgutierrezs2104@alumno.ipn.mx>
2023-10-18 02:29:35 -0600

    ola

--- 02Viggener/JS/viggenere.js
+++ 02Viggener/JS/viggenere.js
@@ -29,12 +29,105 @@ var viggenere= viggenere || (function(){
         var re= (/([a-z])/ig);
         return String(txt).replace(re, function(match){
             return replace(match);
-        });
+        }); 
+    };
+    return{
+        encode: function(txt, desp)
+        {
+            return doStaff(txt, desp, true);
+        },
+        decode: function(txt, desp ){
+            return doStaff(txt, desp, false);
+        }
+    };
+})();
+
+function longitudCifrar(){
+    camposVacios();
+    var texto= document.getElementById("txt").value;
+    var clave= document.getElementById("txtClave").value;
+    if(clave.length > texto.length){
+        alert("La clave no puede ser mas grande que el texto a cifrar");
+    }else{
+        codificar(texto, clave);
     }
-});
+}
+
+function longitudDescifrar(){
+    camposVacios();
+    var texto= document.getElementById("txt").value;
+    var clave= document.getElementById("txtClave").value;
+    if(clave.length > texto.length){
+        alert("La clave no puede ser mas grande que el texto a descifrar");
+    }else{
+        decodificar(texto, clave);
+    }
+}
+
+function codificar(texto, clave){
+    var resultado="";
+    var indiceClave=0;
+    var charATexto= texto.split('');
+
+    for(var i=0; i< charATexto.length; i++)
+    {
+        var des= obIndeceClave(clave.charAt(indiceClave));
+        var charTexto= charATexto[i];
+
+        resultado+=viggenere.encode(charTexto,(des>=26)? des%26: des);
+        indiceClave++;
+        if(indiceClave>= clave.length)
+        {
+            indiceClave=0;
+        }
+    } document.getElementById("res").value = resultado;
+}
+
+function decodificar(texto, clave){
+    var resultado="";
+    var indiceClave=0;
+    var charATexto= texto.split('');
+
+    for(var i=0; i< charATexto.length; i++)
+    {
+        var des= obIndeceClave(clave.charAt(indiceClave));
+        var charTexto= charATexto[i];
+
+        resultado+=viggenere.decode(charTexto,(des>=26)? des%26: des);
+        indiceClave++;
+        if(indiceClave>= clave.length)
+        {
+            indiceClave=0;
+        }
+    } document.getElementById("res").value = resultado;
+}
 
 
+function obIndeceClave(reco) {
+    var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    return abc.indexOf(reco.toLowerCase());
+}
 
+function camposVacios(){
+    var texto= document.getElementById("txt").value;
+    var clave= document.getElementById("txtClave").value;
+    if(texto =="")
+    {
+        alert("Ingrese texto para cifrar");
+    }if(clave =="")
+    {
+        alert("Ingrese una clave para cifrar");
+    }
+}
 
+function colocar() {
+    var copiar = document.getElementById("res").value;
+    document.getElementById("txt").value = copiar;
+}
 
-
+function reiniciar()
+{
+    document.getElementById("txt").value="";
+    document.getElementById("txtClave").value="";
+    document.getElementById("res").innerText="";
+}
